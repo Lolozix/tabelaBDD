7ce8762b4b472840ee554ed8787cfc27582e98f3
Lorrana Damasceno <131817254+Lolozix@users.noreply.github.com>
2024-11-17 14:51:17 -0400

    Add files via upload

--- lolozix.js
+++ lolozix.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = 8080;
+require('dotenv').config();
+const port = 5000;
 app.use(express.json());
 
 //Aki começa a conexão do banco de dados com o expresse com a senha do meu postgre,usuário,localhost e o banco 
 const pool = new Pool({
-  connectionString: "postgres://postgres:aluno@localhost:5432/lorrana2A",
-})
-
+  connectionString: process.env.DATABASE_URL,
+});
 // Rota para a raiz
 app.get('/usuarios', (req, res) => {
   res.send('Rodando o BACKEND com BANCO DE DADOS');
