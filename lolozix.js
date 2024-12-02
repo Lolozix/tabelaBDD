cc6d48b6364de746afbd5d9602251e6bbf1f0628
lolozix <lorranasilvadamasceno@gmail.com>
2024-12-02 13:07:55 -0400

    commit

--- lolozix.js
+++ lolozix.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const port = 5000;
 app.use(express.json());
 
-//Aki começa a conexão do banco de dados com o expresse com a senha do meu postgre,usuário,localhost e o banco 
+//Começa a conexão do banco de dados com o expresse com a senha do meu postgre,usuário,localhost e o banco 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
@@ -17,12 +17,12 @@ app.get('/usuarios', (req, res) => {
 // Rota para listar usuários
 app.get('/', async (req, res) => {
   try {
-      //O metodo query apresenta os dados do banco,ele executa comandos SQL no exemplo utilizei o * para listar todos os dados dos usuários
-    const result = await pool.query('SELECT * FROM projeto_bancodedados');
+    //O metodo query apresenta os dados do banco,ele executa comandos SQL no exemplo utilizei o * para listar todos os dados dos usuários
+    const result = await pool.query('SELECT * FROM tabelona');
     res.json(result.rows);
   } catch (error) {
-    console.error('Erro ao buscar dados, é agora Silver:', error);
-    res.status(500).json({ error: 'Erro ao buscar dados, é agora Silver' });
+    console.error('Erro ao buscar dados, e agora Silver?:', error);
+    res.status(500).json({ error: 'Erro ao buscar dados, e agora Silver?' });
   }
 });
 
